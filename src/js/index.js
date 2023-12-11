ae62b5da1c9ff21fd6c7afb095d838b4a3085186
Kaua2123 <kauantnetsoares@gmail.com>
2023-12-11 20:01:20 -0300

    exibindo todos os pokemons iniciais

--- src/js/index.js
+++ src/js/index.js
@@ -1,87 +1,111 @@
-const resultado = document.querySelector('.resultado');
-const namePokemon = document.querySelector('.name-pokemon');
-const abilitiesPokemon = document.querySelector('.abilities-pokemon');
 
-const pokemonsIniciais = () => {
-    
-    //if selecionou pikachu - chama pikachu...
-    pikachu();  
-}
+(function () {
+    const namePokemon = document.querySelector('.name-pokemon');
+    const pokemon = document.querySelector('.pokemon'); //IMAGEM 
+    const abilitiesPokemon = document.querySelector('.abilities-pokemon');
 
-const pikachu = () => {
-    fetch('https://pokeapi.co/api/v2/pokemon/pikachu/') 
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-        console.log(response.abilities[0].ability.name);
-        namePokemon.innerHTML = response.name.toUpperCase();
-        const abilities = response.abilities.map((valor, indice) => {
-            return valor.ability.name
+
+    const exibePokemonSelecionado = () => {
+
+        const selectPokemon = document.querySelector('.select-pokemon'); //VALOR DO SELECT
+        selectPokemon.addEventListener('change', (e) => {
+            if (selectPokemon.value === 'charmander') {
+                pokemon.setAttribute('src', 'charmander.png')
+                charmander(); 
+                console.log('selecionado: ', selectPokemon.value)
+            }
+            else if (selectPokemon.value === 'bulbasaur') {
+                pokemon.setAttribute('src', 'bulbasaur.png')
+                bulbasaur();
+                console.log('selecionado: ', selectPokemon.value)
+            }
+            else if (selectPokemon.value === 'squirtle') {
+                pokemon.setAttribute('src', 'squirtle.png')
+                squirtle();
+                console.log('selecionado: ', selectPokemon.value)
+            }
+            else if (selectPokemon.value === 'pikachu') {
+                pokemon.setAttribute('src', 'pikachu.png')
+                pikachu();
+                console.log('selecionado: ', selectPokemon.value)
+            }
         })
-        console.log(abilities)
-        abilitiesPokemon.innerHTML = abilities;
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-}
 
-const charmander = () => {
-    fetch('https://pokeapi.co/api/v2/pokemon/charmander/') 
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-        console.log(response.abilities[0].ability.name);
-        namePokemon.innerHTML = response.name.toUpperCase();
-        const abilities = response.abilities.map((valor, indice) => {
-            return valor.ability.name
-        })
-        console.log(abilities)
-        abilitiesPokemon.innerHTML = abilities;
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-}
+    }
 
-const bulbasaur = () => {
-    fetch('https://pokeapi.co/api/v2/pokemon/bulbasaur/') 
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-        console.log(response.abilities[0].ability.name);
-        namePokemon.innerHTML = response.name.toUpperCase();
-        const abilities = response.abilities.map((valor, indice) => {
-            return valor.ability.name
-        })
-        console.log(abilities)
-        abilitiesPokemon.innerHTML = abilities;
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-}
+    const pikachu = () => {
+        fetch('https://pokeapi.co/api/v2/pokemon/pikachu/')
+            .then((response) => {
+                return response.json();
+            })
+            .then((response) => {
+                namePokemon.innerHTML = response.name.toUpperCase();
+                const abilities = response.abilities.map((valor, indice) => {
+                    return valor.ability.name
+                })
+                console.log(abilities)
+                abilitiesPokemon.innerHTML = abilities;
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
 
-const squirtle = () => {
-    fetch('https://pokeapi.co/api/v2/pokemon/squirtle/') 
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-        console.log(response.abilities[0].ability.name);
-        namePokemon.innerHTML = response.name.toUpperCase();
-        const abilities = response.abilities.map((valor, indice) => {
-            return valor.ability.name
-        })
-        console.log(abilities)
-        abilitiesPokemon.innerHTML = abilities;
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-}
+    const charmander = () => {
+        fetch('https://pokeapi.co/api/v2/pokemon/charmander/')
+            .then((response) => {
+                return response.json();
+            })
+            .then((response) => {
+                namePokemon.innerHTML = response.name.toUpperCase();
+                const abilities = response.abilities.map((valor, indice) => {
+                    return valor.ability.name
+                })
+                console.log(abilities)
+                abilitiesPokemon.innerHTML = abilities;
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
 
-pokemonsIniciais();
+    const bulbasaur = () => {
+        fetch('https://pokeapi.co/api/v2/pokemon/bulbasaur/')
+            .then((response) => {
+                return response.json();
+            })
+            .then((response) => {
+                namePokemon.innerHTML = response.name.toUpperCase();
+                const abilities = response.abilities.map((valor, indice) => {
+                    return valor.ability.name
+                })
+                console.log(abilities)
+                abilitiesPokemon.innerHTML = abilities;
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    const squirtle = () => {
+        fetch('https://pokeapi.co/api/v2/pokemon/squirtle/')
+            .then((response) => {
+                return response.json();
+            })
+            .then((response) => {
+                namePokemon.innerHTML = response.name.toUpperCase();
+                const abilities = response.abilities.map((valor, indice) => {
+                    return valor.ability.name
+                })
+                console.log(abilities)
+                abilitiesPokemon.innerHTML = abilities;
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    exibePokemonSelecionado();
+
+
+})()
